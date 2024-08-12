a93c1d41d67fe6ba51a63b2c39ca005afc5f48f7
thanxx <key900@gmail.com>
2024-08-12 17:02:14 +0000

    add

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "gke_cluster" {
   source         = "github.com/thanxx/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 1
+  GKE_NUM_NODES  = 2
 
 }
 
