41a8e41064e2c462e2d7cbb89fa16ff9860322d1
Oleksandr Radchenko <oleksandr.radchenko@microfocus.com>
2024-08-12 17:26:49 +0200

    make it work

--- main.tf
+++ main.tf
@@ -1,20 +1,36 @@
-terraform {
-  backend "gcs" {
-    bucket = "thanxx-tfstate"
-    prefix = "terraform/state"
-  }
-}
-  
 
-resource "tls_private_key" "this" {
-  algorithm = var.algorithm
-  ecdsa_curve = var.ecdsa_curve
+module "tls_private_key" {
+  source = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
 }
 
+# module "kind_cluster" {
+#   source = "github.com/den-vasyliev/tf-kind-cluster"
+# }
 
 module "gke_cluster" {
   source         = "github.com/thanxx/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES  = 1
+
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/thanxx/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  github_token      = var.GITHUB_TOKEN
+  #config_path       = module.kind_cluster.kubeconfig
+  config_path       = module.gke_cluster.kubeconfig
+}
+
+module "github_repository" {
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+
 }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,18 @@ variable "GOOGLE_REGION" {
 variable "GOOGLE_PROJECT" {
 	type = string
 }
+
+
+variable "GITHUB_OWNER" {
+	type = string
+}
+
+variable "GITHUB_TOKEN" {
+	type = string
+}
+
+variable "FLUX_GITHUB_REPO" {
+	type = string
+}
+
+
